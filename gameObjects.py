12b900ff9fd3310fffcaea7d8c127e19fc020745
punk_na_prekole <58171263+PunkNaPrekole@users.noreply.github.com>
2024-07-14 01:33:59 +0300

    through blood and tears updated to the working version

--- gameObjects.py
+++ gameObjects.py
@@ -1,41 +1,38 @@
-from dataclasses import dataclass
-from typing import Dict, Any, List
+from dataclasses import dataclass, field
+from typing import List, Dict, Optional
 
 
 @dataclass
-class Cargo:
-    name: str
-    color_led: list[int]
+class CargoSettings:
+    balls_loading: int
+    balls_unloading: float
+    color_led: List[int]
+    str_name: str
 
 
 @dataclass
-class Pioneer:
-    id: int
-    filter: List = None
-    home_object: str = None
-    altitude: float = None
-    position: List = None
-    bonus_list: List = None
-    cargo_color: List = None
-    health: int = 100
-    is_cargo: bool = False
-    is_shooting: bool = False
-    num_bullets: int = 0
-    state: str = "landed_home"
-    velocity: Any = None
+class RolePlayer:
+    custom_settings: Dict
+    name_role: str
 
-    def update(self, pioneer: Dict):
-        self.position = pioneer['current_pos']
-        self.bonus_list = pioneer['data_role']['bonus_list']
-        self.cargo_color = pioneer['data_role']['cargo_color']
-        self.health = pioneer['health']
-        self.is_cargo = pioneer['is_cargo']
-        self.is_shooting = pioneer['is_shooting']
-        self.num_bullets = pioneer['num_bullet']
 
-    def update_state(self, state: str, velocity: Any):
-        self.state = state
-        self.velocity = velocity
+@dataclass
+class Player:
+    description: str
+    filter: List[str]
+    home_object: str
+    method_control_obj: str
+    name_player: str
+    robot: str
+    role_player: RolePlayer
+
+
+@dataclass
+class Team:
+    city_team: str
+    color_team: List[int]
+    name_team: str
+    players: List[Player]
 
 
 @dataclass
@@ -43,48 +40,85 @@ class VisInfo:
     color: List[int]
     description: str
 
-    def update(self, vis_info: Dict):
-        self.color = vis_info['color']
-        self.description = vis_info['description']
-
-
-@dataclass()
-class DataRole:
-    current_cargo_color: List[List]
-    current_conditions: int
-    is_cargo: bool
-    num_cargo: int
-
-    def update(self, data_role: Dict):
-        self.is_cargo = data_role['is_cargo']
-        self.num_cargo = data_role['num_cargo']
-        self.current_conditions = data_role['current_conditions']
-        self.current_cargo_color = data_role['current_cargo_color']
-
 
 @dataclass
 class Polygon:
-    id: int
-    role: str
     custom_settings: Dict
-    position: List[float]
+    ind_for_led_controller: Optional[int]
+    position: List
+    role: str
     vis_info: VisInfo
-    data_role: DataRole = None
-
-    def update(self, data_role: Dict, vis_info: Dict):
-        self.data_role = self.data_role.update(data_role)
-        self.vis_info = self.vis_info.update(vis_info)
 
 
+@dataclass
+class Robot:
+    control_obj: str
+    filter_position: Dict
+    ip: str
+    port: int
+    title: str
 
-@dataclass()
-class Team:
-    name: str
-    color: List
-    city: str
-    players: List
-    score: Any  # тут хз int or float
 
-    def update_players(self, players: List):
-        for i, player in enumerate(self.players):
-            player.update(players[i])
+@dataclass
+class Config:
+    cargo_settings: List[CargoSettings]
+    game_description: str
+    game_id: int
+    time_game: float
+
+
+@dataclass
+class InitGameData:
+    config: Config
+    player_manager: List[Team]
+    polygon_manager: List[Polygon]
+    robot_manager: Dict[str, Robot]
+    spare_robots: List
+
+
+@dataclass
+class PlayerInfo:
+    altitude: float
+    balls_command: int
+    balls_user: int
+    centre_info: str
+    centre_value: int
+    current_pos: List[float]
+    data_object: Dict
+    data_role: Dict
+    full_delay: int
+    id: int
+    speed: int
+    time_game: str
+
+
+@dataclass
+class TeamInfo:
+    city_team: str
+    color_team: List[int]
+    name_team: str
+    players: List[PlayerInfo]
+
+
+@dataclass
+class PolygonInfo:
+    current_pos: List
+    data_role: Optional[Dict]
+    name_role: str
+    vis_info: VisInfo
+
+
+@dataclass
+class ServerInfo:
+    gameTime: str
+    game_description: str
+    state: int
+    state_old: int
+    version: str
+
+
+@dataclass
+class InGameData:
+    players_info: List[TeamInfo]
+    polygon_manager: List[PolygonInfo]
+    server_info: ServerInfo
--- parser.py
+++ parser.py
@@ -1,82 +1,93 @@
 import json
-import typing
 from gameObjects import *
 
 
-@dataclass
-class Parser:
-    file_init: typing.Union[dict, None] = None
-    file_in: typing.Union[dict, None] = None
+def load_init_game_data(filepath: str) -> InitGameData:
+    with open(filepath, 'r') as file:
+        data = json.load(file)
 
-    def read_init(self, file_name):
-        with open(file_name) as file:
-            j = json.load(file)
-        self.file_init = j['config']
+    config_data = data['config']
 
-    def read_in(self, file_name):
-        with open(file_name) as file:
-            j = json.load(file)
-        self.file_in = j
+    cargo_settings = [CargoSettings(**cargo) for cargo in config_data['Game_mechanics']['cargo_settings']]
+    game_description = config_data['Game_settings']['game_description']
+    game_id = config_data['Game_settings']['game_id']
+    time_game = config_data['Game_settings']['time_game']
 
-    def get_polygons(self):
-        polygons = [Polygon(
-            id=k,
-            role=v["role"],
-            custom_settings=v['custom_settings'],
-            position=v["position"],
-            vis_info=VisInfo(
-                            color=v['vis_info']['color'],
-                            description=v['vis_info']['description']
-            )) for k, v in self.file_init['config']['Polygon_manager'].items()]
-        return polygons
+    config_obj = Config(
+        cargo_settings=cargo_settings,
+        game_description=game_description,
+        game_id=game_id,
+        time_game=time_game
+    )
 
-    def get_teams(self):
-        teams = [Team(
-            name=team['name_team'],
-            color=team['color_team'],
-            city=team['city_team'],
-            players=[Pioneer(
-                filter=p['filter'],
-                home_object=p['home_object'],
-                id=int(p['robot'])-1
-            ) for p in team['players']],
-            score=0
-        )for team in self.file_init['config']['Player_manager']]
-        return teams
+    player_manager = []
+    for team in config_data['Player_manager']:
+        players = [Player(
+            description=player['description'],
+            filter=player['filter'],
+            home_object=player['home_object'],
+            method_control_obj=player['method_control_obj'],
+            name_player=player['name_player'],
+            robot=player['robot'],
+            role_player=RolePlayer(**player['role_player'])
+        ) for player in team['players']]
+        player_manager.append(Team(
+            city_team=team['city_team'],
+            color_team=team['color_team'],
+            name_team=team['name_team'],
+            players=players
+        ))
 
-    def parse_teams(self):
-        teams = [Team(
-            name=team['name_team'],
-            color=team['color_team'],
-            city=team['city_team'],
-            players=[Pioneer(
-                position=p['current_pos'],
-                bonus_list=p['data_role']['bonus_list'],
-                id=p['id'],
-                health=['data_role']['health'],
-                is_cargo=['data_role']['is_cargo'],
-                is_shooting=['data_role']['is_shooting'],
-                num_bullets=['data_role']['num_bullet']
-            ) for p in team['players']],
-            score=0
-        ) for team in self.file_in['players_info']]
-        return teams
+    polygon_manager = [Polygon(
+        custom_settings=v.get('custom_settings', {}),
+        ind_for_led_controller=v.get('ind_for_led_controller'),
+        position=v.get('position', []),
+        role=v['role'],
+        vis_info=VisInfo(**v['vis_info'])
+    ) for v in config_data['Polygon_manager'].values()]
 
-    def parse_polygons(self):
-        polygons = [Polygon(
-            id=k,
-            role=v["role"],
-            custom_settings=v['custom_settings'],
-            position=v["position"],
-            vis_info=VisInfo(
-                color=v['vis_info']['color'],
-                description=v['vis_info']['description']
-            )) for k, v in self.file_init['config']['Polygon_manager'].items()]
-        return polygons
+    robot_manager = {k: Robot(**v) for k, v in config_data['Robot_manager'].items()}
 
-    def update_game_state(self, teams, polygons):
-        for team in teams:
-            team.update_players(self.parse_teams())
-        for polygon in polygons:
-            polygon.update(self.parse_polygons())
-        pass
+    return InitGameData(
+        config=config_obj,
+        player_manager=player_manager,
+        polygon_manager=polygon_manager,
+        robot_manager=robot_manager,
+        spare_robots=config_data['Spare_robots']
+    )
+
+
+def load_in_game_data(filepath: str) -> InGameData:
+    with open(filepath, 'r') as file:
+        data = json.load(file)
+
+    players_info = []
+    for team in data['players_info']:
+        players = [PlayerInfo(**player) for player in team['players']]
+        players_info.append(TeamInfo(
+            city_team=team['city_team'],
+            color_team=team['color_team'],
+            name_team=team['name_team'],
+            players=players
+        ))
+
+    polygon_manager = []
+    if 'polygon_manager' in data:
+        polygon_manager = [PolygonInfo(
+            current_pos=v['current_pos'],
+            data_role=v.get('data_role'),
+            name_role=v['name_role'],
+            vis_info=VisInfo(**v['vis_info'])
+        ) for v in data['polygon_manager'].values()]
+
+    server_info = ServerInfo(**data['server_info'])
+
+    return InGameData(
+        players_info=players_info,
+        polygon_manager=polygon_manager,
+        server_info=server_info
+    )
+
+
+init_game_data = load_init_game_data('../jsons/init_game.json')
+in_game_data = load_in_game_data('../jsons/in_game.json')
